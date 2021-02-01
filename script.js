e82b271e940cf01ff9645867281838fa5ae7304e
Abraham <43353411+Rezatachi@users.noreply.github.com>
2021-02-01 18:28:31 -0500

    grid-fixed

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //LOADER
 
 
-  const loader = document.querySelector(".loading-wrapper");
-  window.onload = function(){
+const loader = document.querySelector(".loading-wrapper");
+window.onload = function(){
+  setTimeout(function(){
+    loader.style.opacity = "0";
     setTimeout(function(){
-      loader.style.opacity = "0";
-      setTimeout(function(){
-        loader.style.display = "none";
-      }, 500);
-    },1500);
-  }
+      loader.style.display = "none";
+    }, 500);
+  },1500);
+}
 
 
 
@@ -21,50 +21,50 @@ let menuOpen = false;
 let menuSize = false;
 menuBtn.addEventListener('click', ()=>{
 navContent.classList.toggle('change');
-    if(!menuOpen && !menuSize) {
-        menuBtn.classList.add('open');
-        menuBtn.classList.add('size');
-        menuOpen = true;
-        menuSize = true;
-    }
-    else {
-        menuBtn.classList.remove('open');
-        menuBtn.classList.remove('size');
-        menuOpen = false;
-        menuSize = false;
-    }
+  if(!menuOpen && !menuSize) {
+      menuBtn.classList.add('open');
+      menuBtn.classList.add('size');
+      menuOpen = true;
+      menuSize = true;
+  }
+  else {
+      menuBtn.classList.remove('open');
+      menuBtn.classList.remove('size');
+      menuOpen = false;
+      menuSize = false;
+  }
 
 });
 
 //writer-text
 var options = {
-    strings: ["<span style='color:red'>Software Developer{}</span>", 
-    "<span style='color:blue'>Photographer</span>", "<span style='color:gray'>Graphic Designer</span>"],
-    typeSpeed: 50,
-    backSpeed: 50,
-    loop: true
-  };
-  var typed = new Typed('.animate', options);
+  strings: ["<span style='color:red'>Software Developer{}</span>", 
+  "<span style='color:blue'>Photographer</span>", "<span style='color:gray'>Graphic Designer</span>"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true
+};
+var typed = new Typed('.animate', options);
 
 
 //Dark Mode Text Changer
 const doptions = {
-    bottom: '64px', // default: '32px'
-    right: 'unset', // default: '32px'
-    left: '32px', // default: 'unset'
-    time: '0.5s', // default: '0.3s'
-    mixColor: '#fff', // default: '#fff'
-    backgroundColor: '#D8D8D8',  // default: '#fff'
-    buttonColorDark: '#100f2c',  // default: '#100f2c'
-    buttonColorLight: '#fff', // default: '#fff'
-    saveInCookies: true, // default: true,
-    label: '🌓', // default: ''
-    autoMatchOsTheme: true // default: true
-  }
-  
-  const darkmode = new Darkmode(doptions);
-  darkmode.showWidget();
-        
+  bottom: '64px', // default: '32px'
+  right: 'unset', // default: '32px'
+  left: '32px', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#D8D8D8',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: true, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(doptions);
+darkmode.showWidget();
+      
 
 
 //Scroll Reveal
@@ -76,5 +76,3 @@ ScrollReveal().reveal('.about-area p', {distance: '100px', delay: 500});
 const aboutBtn = document.querySelector('.aboutme-btn');
 const main = document.querySelector('.main');
 const about = document.querySelector('.about');
-
-   
