0bf699cbb3002d8c4cf4cf1e85a14ea092233ccc
Abraham <43353411+Rezatachi@users.noreply.github.com>
2021-02-03 22:23:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -65,8 +65,6 @@ const doptions = {
 const darkmode = new Darkmode(doptions);
 darkmode.showWidget();
       
-//Remove #
-$("a").attr("href",$("a").attr("href").replace(/#/, "")); 
 
 //Scroll Reveal
 ScrollReveal().reveal('.about-area h1', {distance: '100px', delay: 250});
